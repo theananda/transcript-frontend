1ff29d510f35dd26aa68288a6af6be16239fca69
DragonVirus <limonster.li@gmail.com>
2019-12-01 00:35:33 +0630

    Add google analytics

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 export default {
 	api_url : 'http://localhost:3000',
+	google_analytic_id : 'UA-86740736-5'
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style.css'
+import VueAnalytics from 'vue-analytics'
+import config from './config/index.js'
+
+Vue.use(VueAnalytics, {
+  id: config.google_analytic_id,
+  router
+});
 
 Vue.config.productionTip = false
 
