fa2aa5bd05506c0a1a0bc6d8030dc0281e7a12f8
DragonVirus <limonster.li@gmail.com>
2019-12-18 17:14:31 +0630

    Highlight searched keyword

--- src/components/Transcript.vue
+++ src/components/Transcript.vue
@@ -8,22 +8,26 @@
               <iconify-icon data-icon="mdi:arrow-left" width="1.5rem" height="1.5rem"></iconify-icon>
               Back to all results
           </router-link>
-          <!-- Add spacer, to align navigation to the right -->
-          <div class="mdl-layout-spacer"></div>
-          <div class="search_warpper" v-if="keyword">
-            Keyword : <strong>{{ keyword }}</strong> 1/20 
-            <iconify-icon data-icon="mdi:chevron-up" width="1.5rem" height="1.5rem"></iconify-icon>
-            <iconify-icon data-icon="mdi:chevron-down" width="1.5rem" height="1.5rem"></iconify-icon>
-          </div>
         </div>
       </header>
       <main class="mdl-layout__content transcript_page">
         <div class="page-content">
            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--12-col">
+                <div class="keyword_warpper" v-if="keyword">
+                  Keyword : <strong>{{ keyword }}</strong> 1/{{ keywordCount }}
+                  <a href="#">
+                    <iconify-icon data-icon="mdi:chevron-up" width="1.5rem" height="1.5rem"></iconify-icon>
+                  </a>
+                  <a href="#">
+                    <iconify-icon data-icon="mdi:chevron-down" width="1.5rem" height="1.5rem"></iconify-icon>
+                  </a>
+                </div>
+              </div>
                <div class="mdl-cell mdl-cell--12-col">
                     <spinner v-if="loading"></spinner>
                     <h1 class="center-title">{{ this.data.title }}</h1>
-                    <div class="transcript_wrapper" v-html=this.data.transcript></div>
+                    <div class="transcript_wrapper" v-html=transcriptText></div>
                </div>
            </div>
         </div>
@@ -36,6 +40,7 @@
 import Axios from 'axios'
 import config from '@/config/index.js'
 import {Jumper} from 'vue-loading-spinner'
+import searchTextHl from 'search-text-highlight'
 
 export default {
     name: 'Transcript',
@@ -47,7 +52,8 @@ export default {
             id : this.$route.params.id,
             data: {},
             keyword: this.$route.params.keyword,
-            loading: true
+            loading: true,
+            jump_position: 1
         }
     },
     mounted() {
@@ -61,7 +67,12 @@ export default {
                 this.data = response.data;
                 this.loading = false;
             });
-        }
+        },
+        /*goToResults(dir) {
+
+          var selector = this.$el.querySelector('qasection');
+          selector.scrollTop;
+        }*/
     },
     computed: {
       backLink() {
@@ -70,7 +81,21 @@ export default {
         } else {
           return { name: 'home' }
         }
+      },
+      transcriptText() {
+        const transcript = this.data.transcript;
+
+        if (this.keyword) {
+          const query = this.keyword;
+          var result = searchTextHl.highlight(transcript, query);
+          return result;  
+        }
+        return transcript;
+      },
+      keywordCount() {
+        return (this.transcriptText.match(/text-highlight/g) || []).length;
       }
+
     }
 }
 
