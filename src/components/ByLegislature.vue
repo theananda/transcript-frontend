14191679e51346916f8e73cfb428fe28b768691c
DragonVirus <limonster.li@gmail.com>
2020-08-09 00:41:30 +0630

    Change list for new data update

--- src/components/ByLegislature.vue
+++ src/components/ByLegislature.vue
@@ -1,170 +1,104 @@
 <template>
-	<div class="content_wrapper mdl-grid">
-		<div class="mdl-cell mdl-cell--6-col legislature_wrapper" v-for="meeting in meetings" v-bind:key="meeting.legislature">
-			<div class="mdl-grid">
-				<div class="mdl-cell mdl-cell--12-col">
-					<h2>{{ meeting.name }}</h2>
-				</div>
-				<div class="mdl-cell mdl-cell--12-col">
-					<div class="term_wrapper" v-for="term in meeting.terms" v-bind:key="term.number">
-						<h4>{{ term.name }}</h4>
-						<div class="mdl-grid">
-							<div class="mdl-cell mdl-cell--4-col" v-for="session in term.sessions" v-bind:key="session.number">
-								<router-link :to="{name: 'browse', 
-													params: {legislature: meeting.legislature, 
-													term: term.number, 
-													session: session.number}}">
-									{{ session.name }}
-								</router-link>
-							</div>		
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="content_wrapper mdl-grid">
+        <div class="mdl-cell mdl-cell--12-col legislature_wrapper" v-for="meeting in meetings" v-bind:key="meeting.legislature">
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--12-col">
+                    <h2>{{ getMeetingName('legislature', meeting.legislature) }}</h2>
+                </div>
+                <div class="mdl-cell mdl-cell--12-col">
+                    <div class="term_wrapper" v-for="term in meeting.terms" v-bind:key="term.number">
+                        <h4>{{ getMeetingName('term', term.number) }}</h4>
+                        <div class="mdl-grid">
+                            <div class="mdl-cell mdl-cell--4-col" v-for="session in term.sessions" v-bind:key="session">
+                                <router-link :to="{name: 'browse', 
+                                                    params: {legislature: meeting.legislature, 
+                                                    term: term.number, 
+                                                    session: session}}">
+                                    {{ getMeetingName('session', session) }}
+                                </router-link>
+                            </div>      
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	
+
+import Axios from 'axios'
+import config from '@/config/index.js'
+import MeetingNames from '@/helpers/MeetingNames.js'
+
 export default {
-	name: 'ByLegislature',
-	data() {
-		return {
-			meetings : [
-				{
-					legislature: 'upper',
-					name : 'အမျိုးသားလွှတ်တော်',
-					terms : [
-						{
-							number : 2,
-							name : 'ဒုတိယလွှတ်တော်သက်တမ်း',
-							sessions : [
-								{
-									number : 1,
-									name : 'ပထမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 2,
-									name : 'ဒုတိယပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 3,
-									name : 'တတိယပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 4,
-									name : 'စတုတ္ထပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 5,
-									name : 'ပဥ္စမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 6,
-									name : 'ဆဋ္ဌမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 7,
-									name : 'သတ္တမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 8,
-									name : 'အဋ္ဌမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 9,
-									name : 'နဝမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 10,
-									name : 'ဒသမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 11,
-									name : '(၁၁)ကြိမ်မြောက်ပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 12,
-									name : '(၁၂)ကြိမ်မြောက်ပုံမှန်အစည်းအဝေး'
-								}
-
-							]
-						}
-					]
-				},
-				{
-					legislature: 'lower',
-					name : 'ပြည်သူ့လွှတ်တော်',
-					terms : [
-						{
-							number : 2,
-							name : 'ဒုတိယလွှတ်တော်သက်တမ်း',
-							sessions : [
-								{
-									number : 1,
-									name : 'ပထမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 2,
-									name : 'ဒုတိယပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 3,
-									name : 'တတိယပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 4,
-									name : 'စတုတ္ထပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 5,
-									name : 'ပဥ္စမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 6,
-									name : 'ဆဋ္ဌမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 7,
-									name : 'သတ္တမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 8,
-									name : 'အဋ္ဌမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 9,
-									name : 'နဝမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 10,
-									name : 'ဒသမပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 11,
-									name : '(၁၁)ကြိမ်မြောက်ပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 12,
-									name : '(၁၂)ကြိမ်မြောက်ပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 13,
-									name : '(၁၃)ကြိမ်မြောက်ပုံမှန်အစည်းအဝေး'
-								},
-								{
-									number : 14,
-									name : '(၁၄)ကြိမ်မြောက်ပုံမှန်အစည်းအဝေး'
-								}
-
-							]
-						}
-					]
-				}
-			]
-		}
-	}
+    name: 'ByLegislature',
+    data() {
+        return {
+            meetings : [
+                {
+                    legislature: 'upper',
+                    terms : [
+                        {
+                            number : 1,
+                            sessions : [1,2,3,4,5,6,7,8,9,10,11,12,13]
+                        },
+                        {
+                            number : 2,
+                            sessions : [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+                        }
+                    ]
+                },
+                {
+                    legislature: 'lower',
+                    terms : [
+                        {
+                            number : 1,
+                            sessions : [1,2,3,4,5,6,7,8,9,10,11,12,13]
+                        },
+                        {
+                            number : 2,
+                            sessions : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+                        }
+                    ]
+                },
+                {
+                    legislature: 'union',
+                    terms : [
+                        {
+                            number: 2,
+                            sessions: [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+    methods: {
+        getSessions(varlegi, varterm) {
+            const api_url = config.api_url + '/meetings/session';
+            Axios.get(api_url, {
+                params : {
+                    legislature : varlegi,
+                    term : varterm
+                }
+            })
+                .then(
+                    (response) => {
+                        return response.data;
+                    }
+                );
+        },
+        getMeetingName(type, val) {
+            if (type === 'legislature') {
+                return MeetingNames.legislature(val);
+            } else if (type === 'term') {
+                return MeetingNames.term(val);
+            } else {
+                return MeetingNames.session(val);
+            }
+        }
+    }
 }
 
 </script>
